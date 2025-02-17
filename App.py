fe5d8306ddb6a2704d119e49943e03cd497de905
GeoIslam <dr.islam.ali@outlook.com>
2025-02-17 09:32:15 +0200

    Update App.py

--- App.py
+++ App.py
@@ -327,7 +327,7 @@ def main():
         load_file()
     elif choice == "Show Input Logs":
         show_input_logs()
-     elif choice == "Fix Logs":
+    elif choice == "Fix Logs":
         fix_logs()   
     elif choice == "Select Logs":
         select_Training_Data()
